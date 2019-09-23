fecfb17f3999409da3b8bd630155a0d798d882d7
kulisung <bryan.sung@gmail.com>
2019-09-23 17:49:40 +0800

    edit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-12">
-            <h3>Welcome to TENSALL Data Center website!!</h3>
+            <h3>Welcome to TENSALL Data Center!!!</h3>
             <h3>Keep Going!!</h3><br>
         <h3><marquee behavior="scroll" direction="left" scrollamount="10"><a href="{{ route('posts.index')}}">最新公告</a></marquee></h3>
         </div>
