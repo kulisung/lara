4674428019d89998cb66c32be5a60291b1cece01
kulisung <bryan.sung@gmail.com>
2020-03-30 10:31:59 +0800

    edit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-12">
             <h3>Welcome to TENSALL Data Center!!!</h3>
-            <h3>Keep Going!!</h3><br>
+            <h3>Keep designing & testing!!</h3><br>
         <h3><marquee behavior="scroll" direction="left" scrollamount="10"><a href="{{ route('posts.index')}}">登入帳號申請及操作方式請洽MIS，公告僅供參考!!</a></marquee></h3>
         </div>
     </div>
